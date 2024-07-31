4522c18653a6991fd9fc39de0c0523442f3de4f9
Jakub <jakub.k.krupinski@gmail.com>
2024-07-31 12:01:01 +0200

    Removed unused assets, added placenta segmentation

--- controller.py
+++ controller.py
@@ -1,6 +1,3 @@
-from collections import defaultdict
-import os
-from pathlib import Path
 import mujoco as mj
 import time
 import numpy as np
@@ -26,16 +23,15 @@ class Controller(MuJoCoBase):
         xml_path = "scene/main.xml"
         super().__init__(xml_path)
 
+        self._init_main_cam()
+        self._init_feto_cam()
+
         self.num_of_accuators = len(self.data.ctrl)
 
-        self.feto_cam = mj.MjvCamera()
         self.render_dims = 256
         self.save_imgs = False
 
-        # self.reached_target = False
         self._init_kinematic_chain()
-
-        self.reset_camera()
 
         self.current_output = np.zeros(self.num_of_accuators)
         self.current_target_joint_values = np.zeros(self.num_of_accuators)
@@ -45,8 +41,22 @@ class Controller(MuJoCoBase):
         self.cam_init = False
         self.last_movement_steps = 0
 
+    def _init_main_cam(self):
+        self.cam.azimuth = 90.0
+        self.cam.elevation = -24.0
+        self.cam.distance = 2.0
+        self.cam.lookat = np.array([0.0, 0.0, 0.0])
+
+    def _init_feto_cam(self):
+        self.feto_cam = mj.MjvCamera()
+
+        camera_name = "eye"
+        camera_id = mj.mj_name2id(self.model, mj.mjtObj.mjOBJ_CAMERA, camera_name)
+
+        self.feto_cam.type = mj.mjtCamera.mjCAMERA_FIXED
+        self.feto_cam.fixedcamid = camera_id
+
     def _init_kinematic_chain(self):
-        # need to change urdf file
         urdf_path = "scene/ufactory_xarm7/xarm.urdf"
         active_links_mask = [
             False,
@@ -62,19 +72,217 @@ class Controller(MuJoCoBase):
             urdf_path, active_links_mask=active_links_mask
         )
 
-    def reset_camera(self):
-        # Set camera configuration
-        self.cam.azimuth = 90.0
-        self.cam.elevation = -24.0
-        self.cam.distance = 2.0
-        self.cam.lookat = np.array([0.0, 0.0, 0.0])
-
     def get_ee_pos(self):
-        a = np.append(0, self.data.qpos)
+        extended_joints = np.append(0, self.data.qpos)
         return (
-            self.ee_chain.forward_kinematics(a)[:3, 3]
-            # + self.model.body("link_base").pos
+            self.ee_chain.forward_kinematics(extended_joints)[:3, 3]
+            + self.model.body("link_base").pos
         )
+
+    def move_joints(
+        self,
+        target,
+        tolerance=0.05,
+        max_steps=10000,
+        render=True,
+    ):
+        """
+        Moves the specified joint group to a joint target.
+
+        Args:
+            group: String specifying the group to move.
+            target: List of target joint values for the group.
+            tolerance: Threshold within which the error of each joint must be before the method finishes.
+            max_steps: maximum number of steps to actuate before breaking
+        """
+
+        steps = 1
+        result = ""
+
+        self.reached_target = False
+        deltas = np.zeros(self.num_of_accuators)
+
+        # Update self target joint values
+        for i in range(self.num_of_accuators):
+            self.current_target_joint_values[i] = target[i + 1]
+
+        while not self.reached_target:
+            current_joint_values = self.data.qpos
+
+            # self.get_image_data(width=200, height=200, show=True)
+
+            for j in range(self.num_of_accuators):
+                self.data.ctrl[j] = self.current_target_joint_values[j]
+
+            for i in range(self.num_of_accuators):
+                deltas[i] = abs(
+                    self.current_target_joint_values[i] - current_joint_values[i]
+                )
+
+            if max(deltas) < tolerance:
+                result = "success"
+                self.reached_target = True
+                break
+
+            if steps > max_steps:
+                result = "max. steps reached: {}".format(max_steps)
+                break
+
+            mj.mj_step(self.model, self.data)
+            steps += 1
+
+            if render:
+                self.render()
+
+                # Swap OpenGL buffers (blocking call due to v-sync)
+                glfw.swap_buffers(self.window)
+
+                # Process pending GUI events, call GLFW callbacks
+                glfw.poll_events()
+
+        self.last_movement_steps = steps
+
+        return result
+
+    def move_ee(self, ee_position):
+        """
+        Moves the robot arm so that the gripper center ends up at the requested XYZ-position,
+        with a vertical gripper position.
+
+        Args:
+            ee_position: List of XYZ-coordinates of the end-effector (ee_link for UR5 setup).
+        """
+
+        contr.model.body("sphere").pos = ee_position
+
+        joint_angles = self.ik(ee_position)
+        if joint_angles is not None:
+            result = self.move_joints(target=joint_angles)
+        else:
+            result = "No valid joint angles received, could not move EE to position."
+            self.last_movement_steps = 0
+
+        return result
+
+    def wait_for_ms(self, duration, render=True):
+        """
+        Holds the current position by actuating the joints towards their current target position.
+
+        Args:
+            duration: Time in ms to hold the position.
+        """
+
+        starting_time = time.time()
+        elapsed = 0
+        while elapsed < duration:
+
+            if render:
+                self.render()
+
+                # Swap OpenGL buffers (blocking call due to v-sync)
+                glfw.swap_buffers(self.window)
+
+                # Process pending GUI events, call GLFW callbacks
+                glfw.poll_events()
+
+            elapsed = (time.time() - starting_time) * 1000
+
+    def ik(self, ee_position):
+        """
+        Method for solving simple inverse kinematic problems.
+        This was developed for top down graspig, therefore the solution will be one where the gripper is
+        vertical. This might need adjustment for other gripper models.
+
+        Args:
+            ee_position: List of XYZ-coordinates of the end-effector (ee_link for UR5 setup).
+
+        Returns:
+            joint_angles: List of joint angles that will achieve the desired ee position.
+        """
+
+        orientation_axis = "Z"
+        target_orientation = [0, 0, -1]
+
+        ee_position_base = ee_position - self.model.body("link_base").pos
+
+        joint_angles = self.ee_chain.inverse_kinematics(
+            ee_position_base,
+            target_orientation=target_orientation,
+            orientation_mode=orientation_axis,
+        )
+
+        prediction = (
+            self.ee_chain.forward_kinematics(joint_angles)[:3, 3]
+            + self.model.body("link_base").pos
+        )
+
+        diff = abs(prediction - ee_position)
+        error = np.sqrt(diff.dot(diff))
+
+        if error <= 0.4:
+            return joint_angles
+
+        print("Failed to find IK solution.")
+        return None
+
+    def render(self):
+        viewport_width, viewport_height = glfw.get_framebuffer_size(self.window)
+        self.render_main(viewport_width, viewport_height)
+        self.render_secondary(viewport_width, viewport_height, frame_counter=0)
+
+    def render_main(self, viewport_width, viewport_height):
+        main_viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
+
+        mj.mjv_updateScene(
+            self.model,
+            self.data,
+            self.opt,
+            None,
+            self.cam,
+            mj.mjtCatBit.mjCAT_ALL.value,
+            self.scene,
+        )
+        mj.mjr_render(main_viewport, self.scene, self.context)
+
+    def render_secondary(self, viewport_width, viewport_height, frame_counter):
+        # Define viewport rectangle position and size
+        pos_x = viewport_width - self.render_dims
+        pos_y = viewport_height - self.render_dims
+        width = self.render_dims
+        height = self.render_dims
+
+        feto_viewport = mj.MjrRect(pos_x, pos_y, width, height)
+
+        mj.mjv_updateScene(
+            self.model,
+            self.data,
+            self.opt,
+            None,
+            self.feto_cam,
+            mj.mjtCatBit.mjCAT_ALL.value,
+            self.scene,
+        )
+
+        # Placeholder for pixel data
+        pixels = np.zeros((height * width * 3, 1), dtype=np.uint8)
+
+        mj.mjr_render(feto_viewport, self.scene, self.context)
+
+        mj.mjr_readPixels(pixels, None, feto_viewport, self.context)
+
+        if self.save_imgs:
+            path = "imgs/img_" + str(frame_counter) + ".png"
+            reshaped_array = pixels.reshape(self.render_dims, self.render_dims, 3)
+            bgr_array = cv.cvtColor(reshaped_array, cv.COLOR_RGB2BGR)
+
+            mask = cv.imread("mask/mask.png")
+            mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
+            masked_img = cv.bitwise_and(bgr_array, bgr_array, mask=mask)
+
+            cv.imwrite(path, masked_img)
+
+        mj.mjr_drawPixels(pixels, None, feto_viewport, self.context)
+
 
     def show_model_info(self):
         """
@@ -136,206 +344,6 @@ class Controller(MuJoCoBase):
                     self.model.cam_mat0[i],
                 )
             )
-
-    def move_to_joint_target(
-        self,
-        target,
-        tolerance=0.05,
-        max_steps=10000,
-        render=True,
-    ):
-        """
-        Moves the specified joint group to a joint target.
-
-        Args:
-            group: String specifying the group to move.
-            target: List of target joint values for the group.
-            tolerance: Threshold within which the error of each joint must be before the method finishes.
-            max_steps: maximum number of steps to actuate before breaking
-        """
-
-        steps = 1
-        result = ""
-
-        self.reached_target = False
-        deltas = np.zeros(self.num_of_accuators)
-
-        a = np.round(target[1:], 3)
-        print("T", a)
-
-        # Update self target joint values
-        for i in range(self.num_of_accuators):
-            self.current_target_joint_values[i] = target[i+1]
-        
-
-        while not self.reached_target:
-            current_joint_values = self.data.qpos
-
-            # self.get_image_data(width=200, height=200, show=True)
-
-            # We still want to actuate all motors towards their targets, otherwise the joints of non-controlled
-            # groups will start to drift
-            for j in range(self.num_of_accuators):
-                self.data.ctrl[j] = self.current_target_joint_values[j]
-
-            for i in range(self.num_of_accuators):
-                deltas[i] = abs(
-                    self.current_target_joint_values[i] - current_joint_values[i]
-                )
-            
-            if max(deltas) < tolerance:
-                result = "success"
-
-                a = np.round(current_joint_values, 3)
-                print("E", a)
-                print(self.get_ee_pos())
-
-                self.reached_target = True
-                break
-
-            if steps > max_steps:
-                result = "max. steps reached: {}".format(max_steps)
-                break
-
-            mj.mj_step(self.model, self.data)
-            steps += 1
-
-            if render:
-                # Get framebuffer viewport
-                viewport_width, viewport_height = glfw.get_framebuffer_size(self.window)
-                self.renderMainScreen(viewport_width, viewport_height)
-                self.renderSecondaryScreen(viewport_width, viewport_height, steps)
-
-                # Swap OpenGL buffers (blocking call due to v-sync)
-                glfw.swap_buffers(self.window)
-
-                # Process pending GUI events, call GLFW callbacks
-                glfw.poll_events()
-
-        self.last_movement_steps = steps
-
-        return result
-
-    def move_ee(self, ee_position):
-        """
-        Moves the robot arm so that the gripper center ends up at the requested XYZ-position,
-        with a vertical gripper position.
-
-        Args:
-            ee_position: List of XYZ-coordinates of the end-effector (ee_link for UR5 setup).
-        """
-
-        joint_angles = self.ik(ee_position)
-        if joint_angles is not None:
-            result = self.move_to_joint_target(target=joint_angles)
-        else:
-            result = "No valid joint angles received, could not move EE to position."
-            self.last_movement_steps = 0
-
-        return result
-
-    def ik(self, ee_position):
-        """
-        Method for solving simple inverse kinematic problems.
-        This was developed for top down graspig, therefore the solution will be one where the gripper is
-        vertical. This might need adjustment for other gripper models.
-
-        Args:
-            ee_position: List of XYZ-coordinates of the end-effector (ee_link for UR5 setup).
-
-        Returns:
-            joint_angles: List of joint angles that will achieve the desired ee position.
-        """
-
-        orientation_axis = "Z"
-        target_orientation = [0, 0, -1]
-
-        # ee_position_base = ee_position - self.model.body("link_base").pos
-
-        joint_angles = self.ee_chain.inverse_kinematics(
-            ee_position,
-            target_orientation=target_orientation,
-            orientation_mode=orientation_axis,
-        )
-
-        prediction = (
-            self.ee_chain.forward_kinematics(joint_angles)[:3, 3]
-            # + self.model.body("link_base").pos
-        )
-
-        print("Target", ee_position)
-        print("Predicition",prediction)
-
-        diff = abs(prediction - ee_position)
-        error = np.sqrt(diff.dot(diff))
-
-        if error <= 0.4:
-            return joint_angles
-
-        print("Failed to find IK solution.")
-        return None
-
-
-    def renderMainScreen(self, viewport_width, viewport_height):
-
-        viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
-
-        mj.mjv_updateScene(
-            self.model,
-            self.data,
-            self.opt,
-            None,
-            self.cam,
-            mj.mjtCatBit.mjCAT_ALL.value,
-            self.scene,
-        )
-        mj.mjr_render(viewport, self.scene, self.context)
-
-    def renderSecondaryScreen(self, viewport_width, viewport_height, frame_counter):
-        # Define viewport recangle position and size
-        pos_x = viewport_width - self.render_dims
-        pos_y = viewport_height - self.render_dims
-        width = self.render_dims
-        height = self.render_dims
-
-        feto_viewport = mj.MjrRect(pos_x, pos_y, width, height)
-
-        # Define fetoscope camera
-        camera_name = "eye"
-        camera_id = mj.mj_name2id(self.model, mj.mjtObj.mjOBJ_CAMERA, camera_name)
-
-        self.feto_cam.type = mj.mjtCamera.mjCAMERA_FIXED
-        self.feto_cam.fixedcamid = camera_id
-
-        mj.mjv_updateScene(
-            self.model,
-            self.data,
-            self.opt,
-            None,
-            self.feto_cam,
-            mj.mjtCatBit.mjCAT_ALL.value,
-            self.scene,
-        )
-
-        # Placeholder for pixel data
-        pixels = np.zeros((height * width * 3, 1), dtype=np.uint8)
-
-        mj.mjr_render(feto_viewport, self.scene, self.context)
-
-        mj.mjr_readPixels(pixels, None, feto_viewport, self.context)
-
-        if self.save_imgs:
-            path = "imgs/img_" + str(frame_counter) + ".png"
-            reshaped_array = pixels.reshape(self.render_dims, self.render_dims, 3)
-            bgr_array = cv.cvtColor(reshaped_array, cv.COLOR_RGB2BGR)
-
-            mask = cv.imread("mask/mask.png")
-            mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
-            masked_img = cv.bitwise_and(bgr_array, bgr_array, mask=mask)
-
-            cv.imwrite(path, masked_img)
-
-        mj.mjr_drawPixels(pixels, None, feto_viewport, self.context)
 
     def display_current_values(self):
         """
@@ -404,35 +412,6 @@ class Controller(MuJoCoBase):
                     self.actuators[i][1], self.sim.data.ctrl[i]
                 )
             )
-
-    def stay(self, duration, render=True):
-        """
-        Holds the current position by actuating the joints towards their current target position.
-
-        Args:
-            duration: Time in ms to hold the position.
-        """
-
-        print('Holding position!')
-        starting_time = time.time()
-        elapsed = 0
-        while elapsed < duration:
-            
-            if render:
-                # Get framebuffer viewport
-                viewport_width, viewport_height = glfw.get_framebuffer_size(self.window)
-                self.renderMainScreen(viewport_width, viewport_height)
-                self.renderSecondaryScreen(viewport_width, viewport_height, 0)
-
-                # Swap OpenGL buffers (blocking call due to v-sync)
-                glfw.swap_buffers(self.window)
-
-                # Process pending GUI events, call GLFW callbacks
-                glfw.poll_events()
-            
-
-            elapsed = (time.time() - starting_time) * 1000
-        print('Moving on...')
 
     def get_image_data(self, show=False, camera="top_down", width=200, height=200):
         """
@@ -528,11 +507,52 @@ class Controller(MuJoCoBase):
         return pos_w
 
 
+def loop():
+    # Define the radius and the center of the circle
+    radius = 0.05
+    center_x = 0
+    center_y = 0
+
+    # Number of points to generate
+    num_points = 20
+
+    # Initialize lists to hold the x and y coordinates
+    x = []
+    y = []
+
+    # Loop through the angles to generate the circle's coordinates
+    for i in range(num_points):
+        theta = 2 * np.pi * i / num_points
+        x=center_x + radius * np.cos(theta)
+        y=center_y + radius * np.sin(theta)
+        pos = [x,y, 0.4]
+        contr.move_ee(pos)
+        # contr.wait_for_ms(1_000)
 
 contr = Controller()
 
-
-pos = [0.3, -0.6, 0.4 ]
+pos = [0.0, 0.0, 0.4]
 contr.move_ee(pos)
-contr.stay(10_000)
+contr.wait_for_ms(1_000)
+
+loop()
+
+# pos = [0.0, 0.02, 0.4]
+# contr.move_ee(pos)
+# contr.wait_for_ms(1_000)
+
+# pos = [0.0, 0.04, 0.4]
+# contr.move_ee(pos)
+# contr.wait_for_ms(1_000)
+
+# pos = [0.02, 0.04, 0.4]
+# contr.move_ee(pos)
+# contr.wait_for_ms(1_000)
+
+# pos = [0.04, 0.04, 0.4]
+# contr.move_ee(pos)
+
+# contr.wait_for_ms(10_000)
+
+
 
