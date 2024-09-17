a89261961fbc9f0751fb41820cde177ed4170880
Jakub <jakub.k.krupinski@gmail.com>
2024-09-17 11:06:36 +0200

    Changed test_random from while loop to for loop

--- tests/test_random.py
+++ tests/test_random.py
@@ -9,9 +9,7 @@ def main():
 
     observations = env.reset()[0]
 
-    run = True
-
-    while run:
+    for _ in range(5):
 
         action = env.action_space.sample()
         observations, reward, terminated, truncated, info = env.step(action)
@@ -20,6 +18,8 @@ def main():
             Actions(action),
             reward,
         )
+
+
 
 
 if __name__ == "__main__":
